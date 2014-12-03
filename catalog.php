e7880e99200190db8f911c85ceddd98f802b1851
Jeff Risberg <jrisberg@aol.com>
2014-12-02 22:58:33 -0800

    first version of filtering

--- catalog.php
+++ catalog.php
@@ -9,8 +9,34 @@
  -->
 
 <?php
-$sql = <<<SQL
-SELECT * FROM champions
+if (array_key_exists('search_type', $_GET))
+    $search_type = $_GET['search_type'];
+else
+    $search_type = "all";
+
+if ($search_type == "myList")
+    $sql = <<<SQL
+SELECT * FROM champions c
+LEFT JOIN user_champion uc ON uc.champion_id = c.id
+WHERE uc.user_id = $user_id
+SQL;
+else if ($search_type == "myCollection")
+    $sql = <<<SQL
+SELECT * FROM champions c
+LEFT JOIN skins s ON s.champion_id = c.id
+LEFT JOIN user_skin_collection usc ON usc.skin_id = s.id
+WHERE usc.user_id = $user_id and usc.ownership_status = 'collected'
+SQL;
+else if ($search_type == "myWishList")
+    $sql = <<<SQL
+SELECT * FROM champions c
+LEFT JOIN skins s ON s.champion_id = c.id
+LEFT JOIN user_skin_collection usc ON usc.skin_id = s.id
+WHERE usc.user_id = $user_id and usc.ownership_status = 'wished'
+SQL;
+else // all
+    $sql = <<<SQL
+SELECT * FROM champions c
 SQL;
 
 if (!$result = mysqli_query($db_connection, $sql)) {
@@ -23,10 +49,13 @@ $num_champions = $result->num_rows;
 <div class="page-body">
     <div class="title-header">
         <select>
-            <option value="all">All</option>
-            <option value="myList">My List</option>
-            <option value="myCollection">My Collection</option>
-            <option value="myWishList">My Wish List</option>
+            <option <?php echo ($search_type == "all") ? "selected" : "" ?> value="all">All</option>
+            <option <?php echo ($search_type == "myList") ? "selected" : "" ?> value="myList">My List</option>
+            <option <?php echo ($search_type == "myCollection") ? "selected" : "" ?> value="myCollection">My
+                Collection
+            </option>
+            <option <?php echo ($search_type == "myWishList") ? "selected" : "" ?> value="myWishList">My Wish List
+            </option>
         </select>
     </div>
     <div class="catalog-list">
