75aed10854f3bbe07d7739b0c8d6e7b1d46e3be7
Jeff Risberg <jrisberg@aol.com>
2014-12-07 04:52:17 -0800

    added tongue to catalog display

--- catalog.php
+++ catalog.php
@@ -16,27 +16,31 @@ else
 
 if ($search_type == "myList")
     $sql = <<<SQL
-SELECT * FROM champions c
+SELECT c.id as id, c.name as name, c.title as title, c.icon_img_url as icon_img_url, r.name as role FROM champions c
+LEFT JOIN champion_roles R ON c.role_id = r.id
 LEFT JOIN user_champion uc ON uc.champion_id = c.id
 WHERE uc.user_id = $user_id
 SQL;
 else if ($search_type == "myCollection")
     $sql = <<<SQL
-SELECT * FROM champions c
+SELECT c.id as id, c.name as name, c.title as title, c.icon_img_url as icon_img_url, r.name as role FROM champions c
+LEFT JOIN champion_roles R ON c.role_id = r.id
 LEFT JOIN skins s ON s.champion_id = c.id
 LEFT JOIN user_skin_collection usc ON usc.skin_id = s.id
 WHERE usc.user_id = $user_id and usc.ownership_status = 'collected'
 SQL;
 else if ($search_type == "myWishList")
     $sql = <<<SQL
-SELECT * FROM champions c
+SELECT c.id as id, c.name as name, c.title as title, c.icon_img_url as icon_img_url, r.name as role FROM champions c
+LEFT JOIN champion_roles R ON c.role_id = r.id
 LEFT JOIN skins s ON s.champion_id = c.id
 LEFT JOIN user_skin_collection usc ON usc.skin_id = s.id
 WHERE usc.user_id = $user_id and usc.ownership_status = 'wished'
 SQL;
 else // all
     $sql = <<<SQL
-SELECT * FROM champions c
+SELECT c.id as id, c.name as name, c.title as title, c.icon_img_url as icon_img_url, r.name as role FROM champions c
+LEFT JOIN champion_roles R ON c.role_id = r.id
 SQL;
 
 if (!$result = mysqli_query($db_connection, $sql)) {
@@ -50,12 +54,14 @@ $num_champions = $result->num_rows;
     <div class="title-header">
         <select id="search_type">
             <option <?php echo ($search_type == "all") ? "selected" : "" ?> value="all">All</option>
-            <option <?php echo ($search_type == "myList") ? "selected" : "" ?> value="myList">My List</option>
-            <option <?php echo ($search_type == "myCollection") ? "selected" : "" ?> value="myCollection">My
-                Collection
-            </option>
-            <option <?php echo ($search_type == "myWishList") ? "selected" : "" ?> value="myWishList">My Wish List
-            </option>
+            <?php if ($b_user_logged_in) { ?>
+                <option <?php echo ($search_type == "myList") ? "selected" : "" ?> value="myList">My List</option>
+                <option <?php echo ($search_type == "myCollection") ? "selected" : "" ?> value="myCollection">My
+                    Collection
+                </option>
+                <option <?php echo ($search_type == "myWishList") ? "selected" : "" ?> value="myWishList">My Wish List
+                </option>
+            <?php } ?>
         </select>
     </div>
     <div class="catalog-list">
@@ -66,11 +72,12 @@ $num_champions = $result->num_rows;
                 echo '<figure>';
                 echo '<div style="width: 120px">';
                 echo '<a href="show_champion.php?id=' . $row['id'] . '">';
-                echo '<img src=' . $row['icon_img_url'] . ' href="" style="align-content: center">';
+                echo '<img src="' . $row['icon_img_url'] . '" style="align-content: center">';
                 echo '</a>';
                 echo '<figcaption style="align-content: center">' . $row['name'] . '</figcaption>';
                 echo '</div>';
                 echo '</figure>';
+                echo '<div class="tongue-content">' . $row['role'] . '<br/>' . $row['title'] . '</div>';
                 echo '</div>';
             }
             ?>
@@ -78,12 +85,15 @@ $num_champions = $result->num_rows;
     </div>
 </div>
 
+<script type="text/javascript" src="js/jquery.tongue.min.js"></script>
+
 <script>
     $(document).ready(function () {
         $('#search_type').change(
             function () {
                 search_type = $('#search_type').val();
                 window.location.href = ('catalog.php?search_type=' + search_type);
-            })
+            });
+        $('.champion-icon').tongue({position: 'top', start_speed: 500, end_speed: 300});
     });
 </script>
