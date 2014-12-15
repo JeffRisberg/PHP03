3c1d1e00f9c31fe548b01fbdcab5efd81d4952c6
Jeff Risberg <jrisberg@aol.com>
2014-12-15 00:25:56 -0800

    added in transaction management

--- profile_friend_request_process.php
+++ profile_friend_request_process.php
@@ -5,7 +5,7 @@
 /**
  * Created by PhpStorm.
  * User: Brandon
- * Date: 12/14/2014
+ * Date: 12/05/2014
  * Time: 11:44 PM
  */
 
@@ -13,12 +13,16 @@ $action = $_GET['action'];
 $friend_id = $_GET['friend_id'];
 
 if ($action == 'accept') {
+    // Begin a transaction
+    mysqli_autocommit($db_connection, FALSE);
+
     $sql = <<<SQL
 INSERT INTO user_friend (user_id, friend_id, date_created)
 VALUES ('$user_id', '$friend_id', now());
 SQL;
 
     if (!$result = mysqli_query($db_connection, $sql)) {
+        mysqli_rollback($db_connection);
         die('There was an error running the query [' . mysqli_error($db_connection) . ']');
     }
 
@@ -28,29 +32,36 @@ VALUES ('$friend_id', '$user_id', now());
 SQL;
 
     if (!$result = mysqli_query($db_connection, $sql)) {
+        mysqli_rollback($db_connection);
         die('There was an error running the query [' . mysqli_error($db_connection) . ']');
     }
 
     $sql = <<<SQL
-UPDATE user_friend_request SET status_id=2, last_updated = now()
-WHERE user_id = $friend_id and friend_id=$user_id
+UPDATE user_friend_request
+SET status_id = 2, last_updated = now()
+WHERE user_id=$friend_id and friend_id=$user_id
+SQL;
+
+    if (!$result = mysqli_query($db_connection, $sql)) {
+        mysqli_rollback($db_connection);
+        die('There was an error running the query [' . mysqli_error($db_connection) . ']');
+    }
+
+    mysqli_commit($db_connection);
+
+} else if ($action == 'ignore') {
+    $sql = <<<SQL
+UPDATE user_friend_request
+SET status_id = 3, last_updated = now()
+WHERE user_id=$friend_id AND friend_id=$user_id
 SQL;
 
     if (!$result = mysqli_query($db_connection, $sql)) {
         die('There was an error running the query [' . mysqli_error($db_connection) . ']');
     }
-
-} else if ($action == 'ignore') {
-    $sql = <<<SQL
-UPDATE user_friend_request
-SET status_id=3, last_updated=now()
-WHERE user_id=$friend_id AND friend_id=$user_id
-SQL;
-
-    if (!$friend_request_result = mysqli_query($db_connection, $sql)) {
-        die('There was an error running the query [' . mysqli_error($db_connection) . ']');
-    }
 }
+
+include '_disconnect.php';
 
 header('Location: profile_list_requests.php');
 ?>
